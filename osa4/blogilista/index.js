271defa0710545dafbdb3420e4a8c9909162803f
Henna <henna.honkanen1@gmail.com>
2020-12-08 02:57:35 +0000

    4.3

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -5,50 +5,6 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-/*
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-*/
-/*
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-/*
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-*/
-
 const PORT = config.PORT
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
