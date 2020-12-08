1b7d15f0b5e4e2823295129a112f028afe921584
Henna <henna.honkanen1@gmail.com>
2020-12-08 17:33:34 +0000

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+  const total = blogs.reduce(reducer, 0)
+  return total;
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
