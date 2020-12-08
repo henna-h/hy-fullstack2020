d54d9cfade9256c04651f1c612992fd8c98bb3ce
Henna <henna.honkanen1@gmail.com>
2020-12-08 02:34:17 +0000

    app.js

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,12 +1,9 @@
+const app = require('./app') 
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+
+const server = http.createServer(app)
 
 /*
 const blogSchema = mongoose.Schema({
@@ -18,7 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 */
-
+/*
 const mongoUrl = config.MONGODB_URI
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
