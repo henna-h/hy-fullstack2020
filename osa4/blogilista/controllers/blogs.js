06d1c6ea16b93c6d5ad3439f893a396a2d8a7284
Henna <henna.honkanen1@gmail.com>
2020-12-10 18:56:13 +0000

    4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,6 +3,16 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const bodyParser = require('body-parser')
 blogsRouter.use(bodyParser.json())
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -14,7 +24,14 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
