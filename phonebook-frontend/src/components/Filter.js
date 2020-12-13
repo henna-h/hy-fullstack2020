f781d4445c3b615df3569847f205db5266051a11
Henna <henna.honkanen1@gmail.com>
2020-12-13 02:37:09 +0000

    puhelinluettelo

--- phonebook-frontend/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = (props) => {
-    return (
-      <div>
-        filter shown with <input value={props.showAll} onChange={props.handleToShowChange} />
-      </div>
-    )
-}
-
-export default Filter
