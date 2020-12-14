07f537d7ff414202537a7d007cb83d5c547366c6
Henna <henna.honkanen1@gmail.com>
2020-12-14 03:53:03 +0000

    5.4

---
+++ osa4/blogilista/build/precache-manifest.6133d025076b492f1a8f5cd180da2e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98c1f832d9bf04cb6c6e5f524685e63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e5b19d34e082db0e94",
+    "url": "/static/css/main.29062a80.chunk.css"
+  },
+  {
+    "revision": "31a8092a79e62ac30611",
+    "url": "/static/js/2.d0606b83.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d0606b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e5b19d34e082db0e94",
+    "url": "/static/js/main.b714e81e.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
