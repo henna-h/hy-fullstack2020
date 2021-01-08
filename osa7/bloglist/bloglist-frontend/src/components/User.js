2ed164c57248b8b1d81ddc9a3ccb8bd83224d859
Henna <henna.honkanen1@gmail.com>
2021-01-08 15:29:00 +0200

    added 7.14

--- osa7/bloglist/bloglist-frontend/src/components/User.js
+++ osa7/bloglist/bloglist-frontend/src/components/User.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const User = ({ user }) => {
+    
+    if (!user) {
+      return null
+    }
+    
     return (
-        <div>
-            <h1>{user.name}</h1>
-        </div>
+      <div>
+        <h1>{user.name}</h1>
+        <h2>added blogs</h2>
+        {user.blogs.map(blog =>
+          <ul key={blog.id}>
+            <li>{blog.title} by {blog.author}</li>
+          </ul>)}
+      </div>
     )
 }
 
