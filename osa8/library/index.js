e561b10cb15e1ef805ce5f4d82b044387a329c50
Henna <henna.honkanen1@gmail.com>
2021-01-24 04:44:37 +0200

    8.5

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,12 +111,21 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      if(!args.author){
+    allBooks: (roots, args) => {
+      if(!args.author && !args.genre){
         return books
       }
+      
+      if(!args.author){
+        return books.filter(b => b.genres.includes(args.genre))
+      }
 
-      return books.filter(b => b.author === args.author)
+      if(!args.genre){
+        return books.filter(b => b.author === args.author)
+      }
+
+      return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
+    
     },
     allAuthors: () => authors
   },
