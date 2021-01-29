3958874a8ac9a654d4b30e92e7245c4a53ff9130
Henna <henna.honkanen1@gmail.com>
2021-01-29 02:19:46 +0200

    8.12

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -64,10 +64,11 @@ const Authors = (props) => {
       <form onSubmit={submit}>
         <div>
           name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map(a => 
+              <option key={a.name}>{a.name}</option>
+            )}
+          </select>
         </div>
         <div>
           born
